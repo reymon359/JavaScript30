21e7d20a8df61c80451371afecefb9320189d9b7
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-08-19 21:59:23 +0200

    toggle play

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,5 +8,10 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 // Build our functions
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
 
 // Hook up the event listners
+video.addEventListener('click', togglePlay);
