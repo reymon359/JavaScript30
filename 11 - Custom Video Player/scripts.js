c9697aea00bca48fb016069888c2e64298dead74
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-08-19 21:56:51 +0200

    GEtting out elements

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,4 +1,11 @@
-// Get our elements
+/* Get Our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 // Build our functions
 
