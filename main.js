6a05d95f6f16ebdf2c6e4673e3cfbfebe1ceb741
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-01-16 20:16:23 +0100

    cards styles added

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const PROJECTS = ["01 - JavaScript Drum Kit", "02 - JS and CSS Clock",
     "03 - CSS Variables", "04 - Array Cardio Day 1",
     "05 - Flex Panel Gallery", "06 - Type Ahead",
-    "07 - Array Cardio Day 2", "08 - Fun with HTML5 Canvas",
-    "09 - Dev Tools Domination", "10 - Hold Shift and Check Checkboxes",
-    "11 - Custom Video Player", "12 - Key Sequence Detection",
-    "13 - Slide in on Scroll", "14 - JavaScript References VS Copying",
-    "15 - LocalStorage", "16 - Mouse Move Shadow",
-    "17 - Sort Without Articles", "18 - Adding Up Times with Reduce",
-    "19 - Webcam Fun", "20 - Speech Detection",
-    "21 - Geolocation", "22 - Follow Along Link Highlighter",
-    "23 - Speech Synthesis", "24 - Sticky Nav",
-    "25 - Event Capture, Propagation, Bubbling and Once", "26 - Stripe Follow Along Nav",
-    "27 - Click and Drag", "28 - Video Speed Controller",
-    "29 - Countdown Timer", "30 - Whack A Mole",
+    // "07 - Array Cardio Day 2", "08 - Fun with HTML5 Canvas",
+    // "09 - Dev Tools Domination", "10 - Hold Shift and Check Checkboxes",
+    // "11 - Custom Video Player", "12 - Key Sequence Detection",
+    // "13 - Slide in on Scroll", "14 - JavaScript References VS Copying",
+    // "15 - LocalStorage", "16 - Mouse Move Shadow",
+    // "17 - Sort Without Articles", "18 - Adding Up Times with Reduce",
+    // "19 - Webcam Fun", "20 - Speech Detection",
+    // "21 - Geolocation", "22 - Follow Along Link Highlighter",
+    // "23 - Speech Synthesis", "24 - Sticky Nav",
+    // "25 - Event Capture, Propagation, Bubbling and Once", "26 - Stripe Follow Along Nav",
+    // "27 - Click and Drag", "28 - Video Speed Controller",
+    // "29 - Countdown Timer", "30 - Whack A Mole",
 ];
 
 
@@ -37,7 +37,7 @@ function loadProjects() {
         else html += `  <div onmouseenter="cardMouseEnter(event)" onmousemove="cardMouseMove(event)" onmouseleave="cardMouseLeave(event)" class="card-wrap">`;
 
         html += `      <div class="card" >`;
-        html += `          <div class="card-bg" style="background-image: url('images/'${project}'.png');"></div>`;
+        html += `          <div class="card-bg" style="background-image: url('images/01 - JavaScript Drum Kit.png');"></div>`;
         html += `          <div class="card-info">`;
         html += `              <h1>${project}</h1>`;
 
